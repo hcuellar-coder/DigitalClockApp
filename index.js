e7439a7269b6319eb98b127164439ae84c81d048
Eddie Cuellar <heriberto.cuellar1329@gmail.com>
2020-08-16 01:04:02 -0500

    Complete code review revisions and added README

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-//Military Time Option Boolean
 var militaryTime = false;
 
-
-//With the help of google w3 resource was able to implement dateSuffix function
-//returns the suffix for a given date
 function dateSuffix(date) {
-
     if (date % 10 == 1 && date != 11) {
         return date + 'st';
     } else if (date % 10 == 2 && date != 12) {
@@ -17,46 +12,41 @@ function dateSuffix(date) {
     }
 }
 
-//Current Time function: on init creates a new Date object then displays the current Date and time(12Hour format). 
-// if the militaryTime boolean is true the time display will change to 24Hour Format/
 function currentTime() {
-    var currentDate = new Date();
+    let currentDate = new Date();
+    let hours = currentDate.getHours();
+    let minutes = currentDate.getMinutes();
+    let seconds = currentDate.getSeconds();
+    let time = document.getElementById('Time');
 
-    //These variables were added to display a value (i.e. Hour, Minute, Second) under 10 with a zero in front. example: 9 -> 09
-    var displayHours = ((currentDate.getHours() - 12) < 10 ? '0' : '') + (currentDate.getHours() - 12);
-    var displaySeconds = (currentDate.getSeconds() < 10 ? '0' : '') + currentDate.getSeconds();
-    var displayMinutes = (currentDate.getMinutes() < 10 ? '0' : '') + currentDate.getMinutes();
+    let displayHours = (hours - 12 <= 0 ? (hours + 12) : ((hours - 12 < 10 ? '0' : '') + (hours)));
+    let militaryHours = (hours < 10 ? '0' : '') + (hours)
+    minutes = (minutes < 10 ? '0' : '') + minutes;
+    seconds = (seconds < 10 ? '0' : '') + seconds;
+    let amPM = hours >= 12 ? 'PM' : 'AM';
 
-    //These variables were added to display the value of Month, the day of the week and the Date(1-31)
-    var displayMonth = currentDate.toLocaleString('default', { month: 'long' });
-    var displayDay = currentDate.toLocaleString('default', { weekday: 'long' });
-    var displayDateSuffix = dateSuffix(currentDate.getDate());
+    let displayTime = `${displayHours}:${minutes}:${seconds} ${amPM}`;
 
+    let displayMilitaryTime = `${militaryHours}:${minutes}:${seconds}`;
 
-    //variable to display of the current Date
-    var displayDate = displayDay + ', ' + displayMonth + ' ' + displayDateSuffix;
+    time.innerHTML = militaryTime ? displayMilitaryTime : displayTime;
 
-    //variable to display of the current time in 12HR Format
-    var displayTime = displayHours + ':' + displayMinutes + ':' + displaySeconds;
-
-    //variable to display of the current time in 24HR Format
-    var displayMilitaryTime = currentDate.getHours() + ':' + displayMinutes + ':' + displaySeconds;
-
-    //updates the innerHTML of the div's with IDs Date and Time
-    document.getElementById('Date').innerHTML = displayDate;
-    document.getElementById('Time').innerHTML = militaryTime ? displayMilitaryTime : displayTime;
+    displayCurrentDate(currentDate);
 };
 
-//The Set Interval function will call the function Current Time once ever second. This will update
-// display on the screen, more apparent when looking at the seconds.
-setInterval(currentTime, 1000);
+function displayCurrentDate(currentDate) {
 
-//changeTimeButton is an eventListener listening for the click of the button on the page
-// when clicked will fire the function changeTime()
-var changeTimeButton = document.getElementById('button').addEventListener('click', changeTime);
+    const displayMonth = currentDate.toLocaleString('default', { month: 'long' });
+    const displayDay = currentDate.toLocaleString('default', { weekday: 'long' });
+    const displayDateSuffix = dateSuffix(currentDate.getDate());
 
-//changeTime is a function that when called will toggle the boolean variable militaryTime.
-function changeTime() {
-    militaryTime = !militaryTime;
+    let displayDate = `${displayDay}, ${displayMonth} ${displayDateSuffix}`;
+    document.getElementById('Date').innerHTML = displayDate;
 }
 
+setInterval(currentTime, 1000);
+
+document.getElementById('button').addEventListener('click', function () {
+    militaryTime = !militaryTime;
+    currentTime();
+});
